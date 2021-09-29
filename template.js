b4ca6c99333fe538e007df093ee2d663abc55841
Ryan Slack <slackticus@gmail.com>
2021-09-29 15:08:01 -0700

    Publish my changes

---
+++ template.js
@@ -0,0 +1,40 @@
+import flag from 'country-code-emoji'
+
+const template = cf => {
+  const emoji = flag(cf.country) || "👋🏻"
+
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="x-ua-compatible" content="ie=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Hello!</title>
+        <link rel="stylesheet" href="https://unpkg.com/modern-css-reset/dist/reset.min.css" />
+        <style>
+          body {
+            background: #eee;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            min-height: 100vh;
+            font-family: sans-serif;
+          }
+          div.container {
+            background: #fff;
+            border-radius: 1rem;
+            padding: 4rem;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="container">
+          <h1>Hello there! You're connecting from ${cf.country} ${emoji}</h1>
+        </div>
+      </body>
+    </html>
+    `
+}
+
+export default template
